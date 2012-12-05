1331fd2d8d6dff06f3c56ce86c27ab2ecbc04914
Fernando Garcia <fefegarcia_1@hotmail.com>
2012-12-05 18:12:39 -0200

    O programa foi dividido em vários arquivo.
    Código das dosadoras concluido.

---
+++ Ferduino aquarium controller/Cosm.ino
@@ -0,0 +1,115 @@
+void check_status()
+{
+
+  if (tempCflag==true)
+  {
+    chiller_status = 100;
+  }
+  else
+  {
+    chiller_status = 0;
+  }
+
+  if (tempHflag==true)
+  {
+    aquecedor_status = 100;
+  }
+  else
+  {
+    aquecedor_status = 0;
+  }
+  if (PHRflag==true)
+  {
+    reator_status = 100;
+  }
+  else
+  {
+    reator_status =0;
+  }
+  if (ORPflag==true)
+  {
+    ozonio_status = 100;
+  }
+  else
+  {
+    ozonio_status = 0;
+  }
+  if (ato==true)
+  {
+    reposicao_status = 100;
+  }
+  else
+  {
+    reposicao_status = 0;
+  }
+  if (nivel_status==false)
+  {
+    niveis_status = 100;
+  }
+  else
+  {
+    niveis_status = 0;
+  }
+  if (falha_tpa == true)
+  {
+    tpa_status = 100;
+  }
+}
+
+void cosm () //--------------- Envia dados para o cosm.com ------------------------------------
+{
+
+  if (millis() - previous_Millis > 65000)    //Envia dados a cada 65s
+  {
+    previous_Millis = millis();
+
+    ether.dnsLookup(website);
+
+    byte sd = stash.create();
+    stash.print("1,"); //Temp. da agua
+    stash.println(tempC,DEC);
+    stash.print("2,"); //Temp. dissipador
+    stash.println(tempH,DEC);
+    stash.print("3,"); //PH do aquario
+    stash.println(PHA, DEC);
+    stash.print("4,"); //PH do reator de calcio
+    stash.println(PHR,DEC);
+    stash.print("5,"); //Densidade
+    stash.println(DEN);
+    stash.print("6,"); //ORP
+    stash.println(ORP);
+    stash.print("7,"); //Status chiller 0 = desligado, 100 = ligado
+    stash.println(chiller_status);
+    stash.print("8,"); //Status aquecedor 0 = desligado, 100 = ligado
+    stash.println(aquecedor_status);
+    stash.print("9,"); //Status reator de calcio 0 = desligado, 100 = ligado
+    stash.println(reator_status);
+    stash.print("10,"); // Status ozonizador 0 = desligado, 100 = ligado
+    stash.println(ozonio_status);
+    stash.print("11,"); //Status reposicao de agua doce 0 = desligado, 100 = ligado
+    stash.println(reposicao_status);
+    stash.print("12,"); //Status niveis 0 = baixo, 100 = normaj
+    stash.println(niveis_status);
+    stash.print("13,"); //Status TPA 0 = desligado, 100 = ligado
+    stash.println(tpa_status);
+    stash.print("14,"); //Potencia dos leds azuis
+    stash.println(LedToPercent(bled_out));
+    stash.print("15,"); //Potencia dos leds brancos
+    stash.println(LedToPercent(wled_out));
+
+    stash.save();
+
+    // generate the header with payload - note that the stash size is used,
+    // and that a "stash descriptor" is passed in as argument using "$H"
+    Stash::prepare(PSTR("PUT http://$F/v2/feeds/$F.csv HTTP/1.0" "\r\n"
+      "Host: $F" "\r\n"
+      "X-PachubeApiKey: $F" "\r\n"
+      "Content-Length: $D" "\r\n"
+      "\r\n"
+      "$H"),
+    website, PSTR(FEED), website, PSTR(APIKEY), stash.size(), sd);
+
+    // send the packet - this also releases all stash buffers once done
+    ether.tcpSend();
+  }
+}
---
+++ Ferduino aquarium controller/Setup.ino
@@ -0,0 +1,98 @@
+//------------------------setup------------------------------
+void setup()
+{
+  Serial.begin(38400);
+  Serial2.begin(38400); //stamps
+  pinMode(ledPinBlue, OUTPUT);
+  pinMode(ledPinWhite, OUTPUT);
+  pinMode(aquecedorPin, OUTPUT);
+  pinMode(chillerPin, OUTPUT);
+  pinMode(alarmPin, OUTPUT);
+  pinMode(fanPin, OUTPUT);
+  pinMode(bomba1Pin, OUTPUT);
+  pinMode(bomba2Pin, OUTPUT);
+  pinMode(bomba3Pin, OUTPUT);
+  pinMode(reatorPin, OUTPUT);
+  pinMode(ozonizadorPin, OUTPUT);
+  pinMode(solenoide1Pin, OUTPUT);
+  pinMode(multiplexadorS0Pin, OUTPUT);
+  pinMode(multiplexadorS1Pin, OUTPUT);
+  pinMode (4, OUTPUT);  //sd card
+  digitalWrite (4, HIGH);
+  pinMode (dosadora1, OUTPUT);
+  pinMode (dosadora2, OUTPUT);
+  pinMode (dosadora3, OUTPUT);
+
+  myGLCD.InitLCD(LANDSCAPE);
+  myGLCD.clrScr();
+
+  myTouch.InitTouch(LANDSCAPE);
+  myTouch.setPrecision(PREC_MEDIUM);
+
+  sensors.begin();     //start up temperature library
+  // set the resolution to 10 bit
+  sensors.setResolution(sensor_agua, 10);
+  sensors.setResolution(sensor_dissipador, 10);
+
+  if(Ethernet_Shield == true)
+  {
+    if (ether.begin(sizeof Ethernet::buffer, mymac) == 0) 
+    {
+      Serial.println( "Failed to access Ethernet controller");
+    }
+    ether.staticSetup(myip,gwip);
+  }
+  // Set the clock to run-mode
+  rtc.halt(false);
+  min_cnt= (t.hour*60)+t.min;
+  ReadFromEEPROM();
+  lertpaEEPROM();
+  lerPHAEEPROM();
+  lerPHREEPROM();
+  lerORPEEPROM();
+  lerDENEEPROM();
+  ler_dosadora_EEPROM();
+  LED_levels_output(); 
+  card.init(SPI_QUARTER_SPEED,chipselect);
+  volume.init(&card);
+  root.openRoot(&volume);
+  mainScreen(true);
+  Open_channel(ph1);
+  delay(50);
+  Serial2.print("e");
+  Serial2.print('\r');
+  delay(1000);
+  Serial2.flush();
+  Serial2.print("L0");
+  Serial2.print('\r');
+  delay(1000);
+  Open_channel(ph2);
+  delay(50);
+  Serial2.print("e");
+  Serial2.print('\r');
+  delay(1000);
+  Serial2.flush();
+  Serial2.print("L0");
+  Serial2.print('\r');
+  delay(1000);
+  Open_channel(orp);
+  delay(50);
+  Serial2.print("e");
+  Serial2.print('\r');
+  delay(1000);
+  Serial2.flush();
+  Serial2.print("L0");
+  Serial2.print('\r');
+  delay(1000);    
+  Open_channel(ec);
+  delay(50);
+  Serial2.print("e");
+  Serial2.print('\r');
+  delay(1000);
+  Serial2.flush();
+  Serial2.print("L0");
+  Serial2.print('\r');
+  delay(1000);
+
+}
+
